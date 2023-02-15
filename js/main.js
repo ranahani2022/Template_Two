aa7e1545218048e2d19dc5ccef304322fa02aa55
Rana Hani <113351630+ranahani2022@users.noreply.github.com>
2023-02-15 18:56:48 +0200

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,52 @@
+
+// change navbar styles on scroll
+
+window.addEventListener('scroll', () => {
+    document.querySelector('nav').classList.toggle
+    ('window-scroll', window.scrollY > 60)
+})
+
+
+//  show/hide faq answer
+
+const faqs = document.querySelectorAll('.faq');
+faqs.forEach(faq => {
+    faq.addEventListener('click', () => {
+        faq.classList.toggle('open');
+
+        //  change icon
+        const icon = faq.querySelector('.faq__icon i')
+        if(icon.className === 'ri-add-line') {
+            icon.className = 'fa-solid fa-minus'
+        }else {
+            icon.className = 'ri-add-line'
+        }
+
+    })
+})
+
+
+// show/hide nav menu
+const menu = document.querySelector('.nav__menu')
+const menuBtn = document.querySelector('#open-menu-btn')
+const closeBtn = document.querySelector('#close-menu-btn')
+
+menuBtn.addEventListener('click', () => {
+    menu.style.display = 'flex'
+    menuBtn.style.display = 'none'
+    closeBtn.style.display = 'inline-block'
+})
+
+// close nav menu
+const closeNav = () => {
+    menu.style.display = 'none'
+    closeBtn.style.display = 'none'
+    menuBtn.style.display = 'inline-block'
+}
+
+closeBtn.addEventListener('click', closeNav)
+
+
+
+
+
